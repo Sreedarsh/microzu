32fa614602ff69e0890c690e8fa3a2a614d7189c
Sreedarsh <sreedarsh88@gmail.com>
2017-04-13 11:29:34 +0530

    Dropdown in membership page

--- app/code/local/Sreedarsh/Hat/controllers/IndexController.php
+++ app/code/local/Sreedarsh/Hat/controllers/IndexController.php
@@ -19,6 +19,36 @@ class Sreedarsh_Hat_IndexController extends Mage_Core_Controller_Front_Action {
         
         $this->renderLayout();
     }
+    
+    public function categoryAction() {
+       echo Mage::app()->getRequest()->getParam('catid');
+       
+       $customer = Mage::getSingleton('customer/session')->getCustomer();
+       
+        $model = Mage::getModel('sreedarsh_hat/tech');
+        $cus_id = $customer->getId();
+        $customer_model = Mage::getModel('customer/customer')->load($cus_id);
+        $technician = $model->load($cus_id,'customer_id');
+    
+        
+        if($technician->getId()){
+            $update_data = array('child_id' => Mage::app()->getRequest()->getParam('catid'));
+        $save = $model->load($cus_id,'customer_id')->addData($update_data); 
+        $customer_model->setTechnicianCategory(Mage::app()->getRequest()->getParam('catid'));
+        $customer_model->save();
+        $save->save();
+        }
+        else{
+        $data = array('child_id' => Mage::app()->getRequest()->getParam('catid'), 'customer_id' => $customer->getId());
+        $save = $model->setData($data);
+        $customer_model->setTechnicianCategory(Mage::app()->getRequest()->getParam('catid'));
+        $customer_model->save();
+        $save->save();
+       
+       
+       
+    }
    
     
+}
 }
